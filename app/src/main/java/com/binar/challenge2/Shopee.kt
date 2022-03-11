751f3822f3972492f94d9fbc944298d64e45acd8
19124-rifaldi <84621245+19124-rifaldi@users.noreply.github.com>
2022-03-11 14:54:52 +0700

    shopee challanges 2,tema,tampilan,fragment,

--- app/src/main/java/com/binar/challenge2/Shopee.kt
+++ app/src/main/java/com/binar/challenge2/Shopee.kt
@@ -2,10 +2,29 @@ package com.binar.challenge2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Shopee : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopee)
+        val shopeeFragment = ShopeeFragment()
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.botNav)
+        bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home->makeCurrentFragment(shopeeFragment)
+
+            }
+            true
+        }
+        makeCurrentFragment(shopeeFragment)
+
+    }
+    private fun makeCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.frame_layout, fragment)
+            commit()
+        }
     }
 }
